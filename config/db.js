b7f0ed85a9922958c30f4dbd4b20129946e7d37e
Tejas Chavan <2025tejasc-ctrl@users.noreply.github.com>
2026-08-14 13:20:32 +0530

    chore: secure database connection by moving credentials to environment variables and updating gitignore

--- config/db.js
+++ config/db.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://tejaschavan19:Tejas@1920@tejas19.sxq09hn.mongodb.net/?appName=Tejas19",
-);
+mongoose.connect(process.env.MONGO_URI);
+
 const db = mongoose.connection;
+
 db.on("connected", () => {
   console.log("Connected to MongoDB");
 });
+
 db.on("disconnected", () => {
   console.log("Disconnected from MongoDB");
 });
+
 db.on("error", (err) => {
-  console.log("Error connecting to MongoDB", err);
+  console.log("Error connecting to MongoDB:", err);
 });
 
 module.exports = db;
