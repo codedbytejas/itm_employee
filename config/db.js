97a51cfc6c029ed26b714c62c8040fb876a2a575
Tejas Chavan <2025tejasc-ctrl@users.noreply.github.com>
2026-08-14 12:03:31 +0530

    refactor: standardize indentation and formatting in MongoDB connection configuration

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://tejaschavan19:Tejas@1920@tejas19.sxq09hn.mongodb.net/?appName=Tejas19')
-    const db = mongoose.connection;
-    db.on("connected", () => {
-        console.log("Connected to MongoDB");
-    });
-    db.on("disconnected", () => {
-        console.log("Disconnected from MongoDB");
-    });
-    db.on("error", (err) => {
-        console.log("Error connecting to MongoDB", err);
-    });
+mongoose.connect(
+  "mongodb+srv://tejaschavan19:Tejas@1920@tejas19.sxq09hn.mongodb.net/?appName=Tejas19",
+);
+const db = mongoose.connection;
+db.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+db.on("disconnected", () => {
+  console.log("Disconnected from MongoDB");
+});
+db.on("error", (err) => {
+  console.log("Error connecting to MongoDB", err);
+});
 
-    module.exports = db;
+module.exports = db;
