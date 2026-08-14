f8739d7153bbb17ce16aa0c276d56525e3f89130
Tejas Chavan <2025tejasc-ctrl@users.noreply.github.com>
2026-08-14 13:27:30 +0530

    feat: add mongodb driver and configure server port via environment variables

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use("/employees", employeeRouter);
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
