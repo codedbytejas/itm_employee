1158545eb5fc06209e591109e34d1d07b4b02e2b
Tejas Chavan <2025tejasc-ctrl@users.noreply.github.com>
2026-08-14 12:01:27 +0530

    style: standardize quotation marks and indentation in server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const db = require("./config/db");
 const employeeRouter = require("./router/employeeRouter");
 
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use("/employees", employeeRouter);
 
 app.listen(4000, () => {
-    console.log("Server is running on port 4000");
-});ls -R
+  console.log("Server is running on port 4000");
+});
